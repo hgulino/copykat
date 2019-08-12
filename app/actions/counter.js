e61d49545b6405f1adf46f2a9e1f13122ac7dabd
Hunter <huntergulino@gmail.com>
2019-08-11 18:27:53 -0700

    Removed missed Flow type checks

--- app/actions/counter.js
+++ app/actions/counter.js
@@ -25,7 +25,7 @@ export function incrementIfOdd() {
   };
 }
 
-export function incrementAsync(delay: number = 1000) {
+export function incrementAsync(delay = 1000) {
   return dispatch => {
     setTimeout(() => {
       dispatch(increment());
